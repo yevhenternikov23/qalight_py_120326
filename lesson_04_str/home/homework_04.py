fd07c3339045693aa867015b20959c1c9f051854
Yevhen Ternikov <yevhenternikov@gmail.com>
2026-03-26 12:00:55 +0200

    Completed tasks for homework_04

--- lesson_04_str/home/homework_04.py
+++ lesson_04_str/home/homework_04.py
@@ -25,49 +25,119 @@ rolling in wealth."""
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
 треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
 
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("\n", " ")
+print(adwentures_of_tom_sawer)
+
 # task 02 ==
 """ Замініть .... на пробіл
 """
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("....", " ")
+print(adwentures_of_tom_sawer)
 
 
 # task 03 ==
 """ Зробіть так, щоб у тексті було не більше одного пробілу між словами.
 """
-
+adwentures_of_tom_sawer = " ".join(adwentures_of_tom_sawer.split())
+print(adwentures_of_tom_sawer)
 
 # task 04
 """ Виведіть, скількі разів у тексті зустрічається літера "h"
 """
-
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.count("h")
+print(adwentures_of_tom_sawer)
 
 # task 05
 """ Виведіть, скільки слів у тексті починається з Великої літери?
 підказка - порахувати кожну велику літеру напр, .count("A") і їх сумму
 """
 
+adwentures_of_tom_sawer = """\
+Tom gave up the brush with reluctance in his .... face but alacrity
+in his heart. And while 
+the late steamer
+"Big Missouri" worked ....
+and sweated
+in the sun,
+the retired artist sat on a barrel in the .... shade close by, dangled his legs,
+munched his apple, and planned the slaughter of more innocents.
+There was no lack of material;
+boys happened along every little while;
+they came to jeer, but .... remained to whitewash. ....
+By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for
+a kite, in good repair;
+and when he played
+out, Johnny Miller bought
+in for a dead rat and a string to swing it with—and so on, and so on,
+hour after hour. And when the middle of the afternoon came, from being a
+poor poverty, stricken boy in the .... morning, Tom was literally
+rolling in wealth."""
+
+count_chars = 0
+for char in adwentures_of_tom_sawer:
+    if char.isupper():
+        count_chars += 1
+print("Загальна кількість слів, які починаютсья з великої літери:", count_chars)
 
 # task 06
 """ Виведіть позицію, на якій слово Tom зустрічається вдруге
 """
+adwentures_of_tom_sawer = """\
+Tom gave up the brush with reluctance in his .... face but alacrity
+in his heart. And while 
+the late steamer
+"Big Missouri" worked ....
+and sweated
+in the sun,
+the retired artist sat on a barrel in the .... shade close by, dangled his legs,
+munched his apple, and planned the slaughter of more innocents.
+There was no lack of material;
+boys happened along every little while;
+they came to jeer, but .... remained to whitewash. ....
+By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for
+a kite, in good repair;
+and when he played
+out, Johnny Miller bought
+in for a dead rat and a string to swing it with—and so on, and so on,
+hour after hour. And when the middle of the afternoon came, from being a
+poor poverty, stricken boy in the .... morning, Tom was literally
+rolling in wealth."""
 
+first_tom = adwentures_of_tom_sawer.find("Tom")
+second_tom = adwentures_of_tom_sawer.find("Tom", first_tom + 1)
+print(f"Позиція, на якій слово 'Tom' зустрічається вдруге: {second_tom}")
 
 # task 07
 """ Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
 Збережіть результат у змінній adwentures_of_tom_sawer_sentences
 """
-adwentures_of_tom_sawer_sentences = None
+
+adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.replace("\n", " ").split(". ")
+print(adwentures_of_tom_sawer_sentences)
 
 # task 08
 """ Виведіть четверте речення з adwentures_of_tom_sawer_sentences.
 Перетворіть рядок у нижній регістр.
 """
 
+adwentures_of_tom_sawer = adwentures_of_tom_sawer_sentences[3].lower()
+print("Четвертим реченням є наступне речення:", adwentures_of_tom_sawer)
 
 # task 09
 """ Перевірте чи починається якесь речення з "By the time".
 """
 
+for sentence in adwentures_of_tom_sawer_sentences:
+    if sentence.strip().startswith("By the time"):
+        print("Знайдено наступне підходяще під умови задачі речення:", sentence)
+        break
+else:
+    print("Підходящого під умови задачі речення не знайдено")
 
 # task 10
 """ Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
 """
+
+last_sentence_word = adwentures_of_tom_sawer_sentences[-1].strip()
+word_count = len(last_sentence_word.split())
+print("Загальна кількість слів останнього речення:", word_count)
