d04035533a0e03ab0b71fad4c1fe6924b25e487a
Yevhen Ternikov <yevhenternikov@gmail.com>
2026-04-14 18:01:32 +0300

    Completed task for homework_8

--- lesson_08_try_exept/home/homework_08.py
+++ lesson_08_try_exept/home/homework_08.py
@@ -18,15 +18,29 @@ sum_numbers_in_list("21")  # ValueError
 def sum_numbers_in_list(string_list: list):
     """Повертає список сум чисел зі списку строк,
     які складаються з чисел, розділених комою."""
+    if not isinstance(string_list, list) or len(string_list) == 0:
+        raise ValueError
+
     result = []
+
     for item in string_list:
         try:
-            pass
-        except ValueError as e:
-            result.append("Не можу це зробити!")
-    
-    return result
+            if not isinstance(item, str):
+                raise AttributeError
 
+            numbers = item.split(",")
+            numbers = [int(num) for num in numbers]
+
+        except AttributeError:
+            result.append("Не можу це зробити! AttributeError")
+
+        except ValueError:
+            result.append("Не можу це зробити!")
+
+        else:
+            result.append(sum(numbers))
+
+    return result
 
 if __name__ == "__main__":
     output = sum_numbers_in_list(["1,2,3", "4,0,6"])
@@ -34,6 +48,11 @@ if __name__ == "__main__":
 
     output = sum_numbers_in_list(["1,2,3", "4/0,6", "asas7,8,9"])
     print(output)
+
+    output = sum_numbers_in_list(["1,2,3,4", 7])
+    print(output)
+
+
     """
     sum_numbers_in_list(["1,2,3", "4,0,6"])  # [6, 10]
     sum_numbers_in_list(["1,2,3", "asas7,8,9", "4,0,6"])  # [6, "Не можу це зробити!", 10]
